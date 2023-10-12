ef83fb35a2f244481b79f895d6d11f849ac1fee4
ElPiod <malickelhadjo07@gamil.com>
2023-10-12 00:32:28 +0000

    new

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+var typed2 = new Typed('#typed2', {
+    strings: [` <h3 class="text-center fs-4">À Propos</h3> <br>
+    Bienvenue sur Otaku~ Fans ! Nous sommes ravis de vous accueillir et de partager avec vous notre passion pour te faire vivre le monde des mangas. Voici un peu plus sur qui nous sommes et ce que nous faisons. <br>
 
+    <span class="fw-bold fs-4">Notre Mission</span> <br>
+    Chez Otaku, notre mission est de de vos faire decouvrir le Monde des mangas(Anime), vous montrez les plus puissances des acteurs . Nous croyons fermement en que cela vous rendrez heureux(se) et trés until et nous nous engageons à fournir à nos visiteurs les meilleurs animés du Monde. <br>
+
+    <span class="fw-bold fs-4"> Notre Équipe</span> <br>
+    Notre équipe est composée de [2] de passionnés de [votre sujet]. Nous sommes des experts en informatique plus précisement en Developpement Web/mobile et nous sommes déterminés à partager notre expertise avec vous. Notre équipe travaille dur pour créer du contenu de haute qualité, des guides utiles et des ressources intéressantes pour nos visiteurs.
+
+    Ce que Vous Trouverez sur Notre Site
+    Sur Otaku ~ Fans, vous trouverez une variété de contenus, notamment :
+
+    Des articles informatifs sur [sujet de votre site].
+    Des guides détaillés pour [objectif de votre site].
+    Des astuces et des conseils pratiques pour [thème de votre site].
+    Une communauté engagée de passionnés avec qui vous pouvez interagir.
+    [Autres types de contenus ou de ressources pertinents].
+    Pourquoi Nous Faisons Ce Que Nous Faisons
+    Nous croyons fermement que [raisons pour lesquelles vous êtes passionnés par votre sujet]. Notre équipe est animée par le désir de [impact positif que vous espérez avoir sur vos visiteurs]. Nous sommes déterminés à vous aider à [objectifs ou avantages pour les visiteurs].`],
+    typeSpeed: 30,
+    backSpeed: 10,
+    cursorChar: '_',
+    fadeOut: true,
+    loop: false
+  })
+
+const garouText = document.getElementById('text-Garou')
+
+garouText.onclick = () => {
+    var typed3 = new Typed('#typed3', {
+        strings: [`Moi c'est Garou je ne me presente pas car <br>
+                j'ai pas le temps bientot je vais combattre <br>
+                Saitama. Donc lit bien  ce que Vegeto <br> 
+                a dit et vous comprendrez merci`],
+        typeSpeed: 50,
+        backSpeed: 10,
+        cursorChar: '_',
+        fadeOut: true,
+        loop: false,
+      })
+}
+
+
+  
