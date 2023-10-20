8a955311e48efec5f71f6779bacadaa1073a9975
ElPiod <malickelhadjo07@gamil.com>
2023-10-20 18:38:34 +0000

    nouveaux

---
+++ mangas.js
@@ -0,0 +1,8 @@
+const infosText = document.getElementById('infos')
+const infosPlus = document.getElementById('infos_plus')
+
+
+infosText.addEventListener('click', () => {
+  infosPlus.style.display = "block"
+})
+
